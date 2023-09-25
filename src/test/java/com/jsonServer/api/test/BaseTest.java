d2183351f5a10ffc6c1354c348ae77d4c83353f0
sharif24 <azizur15-2124@diu.edu.bd>
2023-09-24 16:47:56 -0800

    first commit

---
+++ src/test/java/com/jsonServer/api/test/BaseTest.java
@@ -0,0 +1,17 @@
+package com.jsonServer.api.test;
+
+
+import io.restassured.RestAssured;
+import org.junit.BeforeClass;
+
+public abstract class BaseTest {
+
+    @BeforeClass
+    public void setUp(){
+        RestAssured.baseURI= "http://localhost";
+//        RestAssured.baseURI= "http://localhost:3000/";
+        RestAssured.port= 3000;
+        RestAssured.basePath= "";
+        RestAssured.enableLoggingOfRequestAndResponseIfValidationFails();
+    }
+}
